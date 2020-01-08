25be99678a379d0c631f696fe0f158e903b13a91
Yurii Starkiv <yuriistarkiv@gmail.com>
2020-01-08 09:32:36 +0000

    Home work part 2

--- index.js
+++ index.js
@@ -7,17 +7,29 @@ function isAdult(age) {
 }
 
 function sortArrayUp(arr) {
-    arr.sort ((a, b) => a - b)
+    arr.sort((a, b) => a - b)
     return arr
 }
 
-function isPalindrome(name){
+function isPalindrome(name) {
     let str = '';
-    for(let i = name.length - 1; i >= 0; i--){
+    for (let i = name.length - 1; i >= 0; i--) {
         str += name[i];
     }
-    if(str.toLowerCase() === name.toLowerCase()) return true
+    if (str.toLowerCase() === name.toLowerCase()) return true
     else return false
 }
 
-module.exports = {isAdult, sortArrayUp, isPalindrome};
+function OddNumber(number) {
+    if (typeof (number) !== 'number') return undefined
+    else if (number % 2 === 1) return true
+    else return false
+}
+
+function EvenNumber(number) {
+    if (typeof (number) !== 'number') return undefined
+    else if (number % 2 === 0) return true
+    else return false
+}
+
+module.exports = {isAdult, sortArrayUp, isPalindrome, OddNumber, EvenNumber};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const {isAdult, sortArrayUp, isPalindrome} = require('../index');
+const {isAdult, sortArrayUp, isPalindrome, OddNumber, EvenNumber} = require('../index');
 
 
 describe('function isAdult', () => {
@@ -53,6 +53,50 @@ describe('function isPalindrome', () => {
 
     it('should function works correct', () => {
         assert.equal(isPalindrome('Anna'), true)
+    })
+})
+
+describe('function OddNumber', () => {
+    it('should function OddNumber return odd number', () => {
+        assert.equal(OddNumber(9), true)
+    })
+
+    it('should function OddNumber return odd number', () => {
+        assert.equal(OddNumber('9'), undefined)
+    })
+
+    it('should function OddNumber return odd number', () => {
+        assert.equal(OddNumber(8), false)
+    })
+
+    it('should function OddNumber return odd number', () => {
+        assert.equal(OddNumber(65), true)
+    })
+
+    it('should function OddNumber return odd number', () => {
+        assert.equal(OddNumber(56), false)
+    })
+})
+
+describe('function EvenNumber', () => {
+    it('should function EvenNumber return even number', () => {
+        assert.equal(EvenNumber(6), true)
+    })
+
+    it('should function EvenNumber return even number', () => {
+        assert.equal(EvenNumber('seven'), undefined)
+    })
+
+    it('should function EvenNumber return even number', () => {
+        assert.equal(EvenNumber(13), false)
+    })
+
+    it('should function EvenNumber return even number', () => {
+        assert.equal(EvenNumber(894), true)
+    })
+
+    it('should function EvenNumber return even number', () => {
+        assert.equal(EvenNumber(999), false)
     })
 })
 
